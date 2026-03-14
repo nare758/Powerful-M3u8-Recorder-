be68dae71d7cbcc370b7a2afb496ec7ae90da8aa
nare758 <podhnaresh31@gmail.com>
2026-03-14 07:40:16 +0530

    Update bot configuration values

--- Config.py
+++ Config.py
@@ -3,21 +3,21 @@
 import os
 
 # Bot token from @botfather
-BOT_TOKEN = "8619959255:AAHq2ekj90Mwx2cI6IdNd-b16JC86YTDMlM"
+BOT_TOKEN = "8799939627:AAF2KrqHD_03cDnnR1nH5Zjtqi5kMRR1IQY"
 
 # From my.telegram.org/
-API_ID = "29481626"
-API_HASH = "4892185769903521077c4cea97808b8c"
+API_ID = "30523194"
+API_HASH = "76d62e302ed767a9ea0b8586a9189ca5"
 
 # For /log cmd
-OWNER_ID = [int(i) for i in os.environ.get("5856009289", "969084369").split(" ")]
+OWNER_ID = [int(i) for i in os.environ.get("8799939627", "7667302446").split(" ")]
 
 # No time limit for this users
-AUTH_USERS = [969084369, 2800583882]
+AUTH_USERS = [7667302446, 2800583882]
 
 # Time gap after each request (in seconds) 
 TIME_GAP = 0
 
-BOT_USERNAME = 'M3u8LiveRecordingBot'  # bot username without @
+BOT_USERNAME = 'Powerful_m3u8_rec_bot'  # bot username without @
 API_TOKEN = '65aa5be4d757fb7242fff9dde00f6cd5d4acc977' #don't change this without developer permission
 
